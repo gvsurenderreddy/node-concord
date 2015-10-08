f0b3c6f2a74fa9d68996092b04e92484d353a000
Mihai Ene <dkrandu@yahoo.com>
2015-10-08 23:16:17 +0100

    Sequence ack.

--- test/sequence.js
+++ test/sequence.js
@@ -38,5 +38,123 @@ describe.only('sequence', () => {
         should.not.exist(values);
         done();
     });
+
+    describe('ack', () => {
+        it('sorted consecutive numbers', (done) => {
+            let seq = new Sequence(5);
+            seq.set(1, '1');
+            seq.set(2, '2');
+            seq.set(3, '3');
+            seq.set(4, '4');
+            seq.set(5, '5');
+            seq.ack[0].should.equal(5);
+            seq.ack.slice(1, 4).forEach((bit) => {
+                bit.should.equal(1);
+            });
+            done();
+        });
+
+        it('sorted numbers with holes', (done) => {
+            let seq = new Sequence(5);
+            seq.set(1, '1');
+            seq.set(2, '2');
+            seq.set(4, '4');
+            seq.set(6, '6');
+            seq.set(7, '7');
+            seq.ack[0].should.equal(7);
+            seq.ack[1].should.equal(1);
+            seq.ack[2].should.equal(0);
+            seq.ack[3].should.equal(1);
+            seq.ack[4].should.equal(0);
+            done();
+        });
+
+        it('sorted numbers with holes and separate extremes', (done) => {
+            let seq = new Sequence(5);
+            seq.set(1, '1');
+            seq.set(3, '3');
+            seq.set(5, '5');
+            seq.set(8, '8');
+            seq.set(11, '11');
+            seq.ack[0].should.equal(11);
+            seq.ack[1].should.equal(0);
+            seq.ack[2].should.equal(0);
+            seq.ack[3].should.equal(1);
+            seq.ack[4].should.equal(0);
+            done();
+        });
+
+        it('reverse sorted consecutive numbers', (done) => {
+            let seq = new Sequence(5);
+            seq.set(1, '1');
+            seq.set(2, '2');
+            seq.set(3, '3');
+            seq.set(4, '4');
+            seq.set(5, '5');
+            seq.ack[0].should.equal(5);
+            seq.ack.slice(1, 4).forEach((bit) => {
+                bit.should.equal(1);
+            });
+            done();
+        });
+
+        it('unsorted consecutive numbers', (done) => {
+            let seq = new Sequence(5);
+            seq.set(4, '4');
+            seq.set(2, '2');
+            seq.set(1, '1');
+            seq.set(5, '5');
+            seq.set(3, '3');
+            seq.ack[0].should.equal(5);
+            seq.ack.slice(1, 4).forEach((bit) => {
+                bit.should.equal(1);
+            });
+            done();
+        });
+
+        it('unsorted consecutive numbers with last largest', (done) => {
+            let seq = new Sequence(5);
+            seq.set(4, '4');
+            seq.set(2, '2');
+            seq.set(1, '1');
+            seq.set(3, '3');
+            seq.set(5, '5');
+            seq.ack[0].should.equal(5);
+            seq.ack.slice(1, 4).forEach((bit) => {
+                bit.should.equal(1);
+            });
+            done();
+        });
+
+        it('unsorted numbers with holes', (done) => {
+            let seq = new Sequence(5);
+            seq.set(2, '2');
+            seq.set(4, '4');
+            seq.set(1, '1');
+            seq.set(7, '7');
+            seq.set(6, '6');
+            seq.ack[0].should.equal(7);
+            seq.ack[1].should.equal(1);
+            seq.ack[2].should.equal(0);
+            seq.ack[3].should.equal(1);
+            seq.ack[4].should.equal(0);
+            done();
+        });
+
+        it('unsorted numbers with holes and separate extremes', (done) => {
+            let seq = new Sequence(5);
+            seq.set(3, '3');
+            seq.set(5, '5');
+            seq.set(11, '11');
+            seq.set(1, '1');
+            seq.set(8, '8');
+            seq.ack[0].should.equal(11);
+            seq.ack[1].should.equal(0);
+            seq.ack[2].should.equal(0);
+            seq.ack[3].should.equal(1);
+            seq.ack[4].should.equal(0);
+            done();
+        });
+    });
 });
 
